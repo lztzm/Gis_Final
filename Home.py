1ba94106418074840bb0e867c31c89fecbfeefdd
lztzm <tsetimie@gmail.com>
2024-12-24 05:18:35 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -49,10 +49,10 @@ st.pydeck_chart(
                 "ColumnLayer",
                 data,
                 get_position="[X, Y]",  # Note: Longitude is X, Latitude is Y
-                get_elevation="Number / 5",  # Set the elevation (height of the column) proportional to 'Number'
+                get_elevation="Number / 10",  # Set the elevation (height of the column) proportional to 'Number'
                 elevation_scale=8000,  # Scale factor for elevation 誇張程度
                 get_color=[0, 0, 255],  # Color of the columns RGBA
-                radius=8000,  # Radius of the columns
+                radius=800000,  # Radius of the columns
                 pickable=True,
             ),
             pdk.Layer(
