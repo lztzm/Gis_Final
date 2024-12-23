7c9eee7d382e93386868e40f11256c53bf51226c
lztzm <tsetimie@gmail.com>
2024-12-24 06:56:55 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -29,4 +29,17 @@ with st.expander("See source code"):
             add_legend=True,
         )
 
+with st.expander("See source code"):
+    with st.echo():
+        filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
+        m = leafmap.Map(center=[40, -100], zoom=4)
+        m.add_heatmap(
+            filepath,
+            latitude="latitude",
+            longitude="longitude",
+            value="pop_max",
+            name="Heat map",
+            radius=20,
+        )
+
 m.to_streamlit(height=700)
