8f260208992eeb988af66fda41f4c2dbf61dc5ce
lztzm <tsetimie@gmail.com>
2024-12-24 07:59:35 +0800

    Update 2_🪟_Split_Map.py

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -15,12 +15,22 @@ st.sidebar.image(logo)
 
 st.title("Split-panel Map")
 
-with st.expander("See source code"):
-    with st.echo():
-        m = leafmap.Map()
-        m.split_map(
-            left_layer="ESA WorldCover 2020 S2 FCC", right_layer="ESA WorldCover 2020"
-        )
-        m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
+heat_data = pd.read_csv("https://raw.githubusercontent.com/lztzm/Gis_Final_Project/refs/heads/main/%E5%90%84%E5%8D%80%E6%99%AF%E9%BB%9E%E6%95%B8%E9%87%8F.csv")
+
+m = leafmap.Map()
+heatmap_layer = leafmap.folium.FeatureGroup(name="熱區地圖")
+    m.add_heatmap(
+        heat_data,
+        latitude="緯度",
+        longitude="經度",
+        value="景點數量",
+        name="Heat map",
+        radius=20,
+    )
+
+m.split_map(
+    left_layer="ESA WorldCover 2020 S2 FCC", right_layer="Heat map"
+)
+m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
 
 m.to_streamlit(height=700)
