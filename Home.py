e92adad3959b374957e8ed595ebbd76fad1330c5
lztzm <tsetimie@gmail.com>
2024-12-24 04:51:14 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -29,34 +29,19 @@ markdown = """
 st.markdown(markdown)
 
 color_map = {
-        "Camino_Frances": [255, 0, 0],           # Vibrant red
-        "Camino_Ingles": [0, 0, 255],           # Strong blue
-        "Camino_Portugues_central": [255, 165, 0],  # Bright orange
-        "Camino_Primitivo": [0, 255, 0],        # Fresh green
-        "Camino_del_Norte": [128, 0, 128],      # Deep purple
-        "Portugues_Coastal": [255, 255, 0],     # Sunny yellow
-        "Via_de_la_Plata": [139, 69, 19],       # Earthy brown
-        "default": [0, 0, 0],                   # Default color if route not found
+        "Japna": [255, 0, 0]               # Default color if route not found
     }
 
 data_urls_dict = {
-    "Camino_Frances": "https://raw.githubusercontent.com/chinchillaZ/streamlit-hw/main/Camino/1_Frances_travelers.csv",
-    "Camino_Ingles": "https://raw.githubusercontent.com/chinchillaZ/streamlit-hw/main/Camino/6_Ingles_travelers.csv",
-    "Camino_Portugues_central": "https://raw.githubusercontent.com/chinchillaZ/streamlit-hw/main/Camino/2_Portugues_travelers.csv",
-    "Camino_Primitivo": "https://raw.githubusercontent.com/chinchillaZ/streamlit-hw/main/Camino/4_Primitivo_travelers.csv",
-    "Camino_del_Norte": "https://raw.githubusercontent.com/chinchillaZ/streamlit-hw/main/Camino/3_Norte_travelers.csv",
-    "Portugues_Coastal": "https://raw.githubusercontent.com/chinchillaZ/streamlit-hw/main/Camino/7_Muxia_travelers.csv",
-    "Via_de_la_Plata": "https://raw.githubusercontent.com/chinchillaZ/streamlit-hw/main/Camino/5_Plata_travelers.csv",
-    "default": ""  # Default key if route is not found
+    "Camino_Frances": "https://raw.githubusercontent.com/lztzm/Gis_Final_Project/refs/heads/main/%E8%A7%80%E5%85%89%E5%AE%A2%E5%9C%8B%E7%B1%8D.csv"
 }
 
 
 def show_map(csv_url, color):
     chart_data = pd.read_csv(csv_url)
-    chart_data = chart_data[chart_data["year"] == 2024]
 
    # Load the GeoJSON data from the URL
-    geojson_url = "https://chinchillaz.github.io/streamlit-hw/all_Camino_route.geojson"
+    geojson_url = "https://raw.githubusercontent.com/lztzm/Gis_Final_Project/refs/heads/main/tourism_comefrom.geojson"
     geojson_data = requests.get(geojson_url).json()
 
     # Filter the GeoJSON features based on the route_name
@@ -104,56 +89,9 @@ def show_map(csv_url, color):
     # Show the table of chart_data
     st.table(chart_data)  # Display the chart data as a table
 
-
-# Create two rows using columns
-upper_row = st.columns(3)  # Upper row with 3 buttons
-lower_row = st.columns(4)  # Lower row with 4 buttons
-
-
-if upper_row[0].button("法國之路", use_container_width=True):
-    route_name = "Camino_Frances"
+    route_name = "Japna"
     data_url = data_urls_dict.get(route_name, data_urls_dict["default"])
     color = color_map.get(route_name, color_map["default"])
     show_map(data_url, color)
-    
-if upper_row[1].button("葡萄牙之路", use_container_width=True):
-    route_name = "Camino_Portugues_central"
-    data_url = data_urls_dict.get(route_name, data_urls_dict["default"])
-    color = color_map.get(route_name, color_map["default"])
-    show_map(data_url, color)
-
-if upper_row[2].button("北方之路", use_container_width=True):
-    route_name = "Camino_del_Norte"
-    data_url = data_urls_dict.get(route_name, data_urls_dict["default"])
-    color = color_map.get(route_name, color_map["default"])
-    show_map(data_url, color)
-
-# Lower row buttons
-if lower_row[0].button("原始之路", use_container_width=True):
-    route_name = "Camino_Primitivo"
-    data_url = data_urls_dict.get(route_name, data_urls_dict["default"])
-    color = color_map.get(route_name, color_map["default"])
-    show_map(data_url, color)
-    
-if lower_row[1].button("銀之路", use_container_width=True):
-    route_name = "Via_de_la_Plata"
-    data_url = data_urls_dict.get(route_name, data_urls_dict["default"])
-    color = color_map.get(route_name, color_map["default"])
-    show_map(data_url, color)
-    
-if lower_row[2].button("英國之路", use_container_width=True):
-    route_name = "Camino_Ingles"
-    data_url = data_urls_dict.get(route_name, data_urls_dict["default"])
-    color = color_map.get(route_name, color_map["default"])
-    show_map(data_url, color)
-    
-if lower_row[3].button("世界盡頭之路", use_container_width=True):
-    route_name = "Portugues_Coastal"
-    data_url = data_urls_dict.get(route_name, data_urls_dict["default"])
-    color = color_map.get(route_name, color_map["default"])
-    show_map(data_url, color)
-
-
-
 
 st.markdown("<br><br><br>", unsafe_allow_html=True)  # Adds three line breaks
