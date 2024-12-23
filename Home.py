01e06921a5212b56e05c40a1a9eda8fe600cee52
lztzm <tsetimie@gmail.com>
2024-12-24 04:02:24 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import pandas as pd
+import geopandas as gpd
 
 st.set_page_config(layout="wide")
 
@@ -22,6 +24,9 @@ markdown = """
 
 st.markdown(markdown)
 
+data = gd.read_csv("https://github.com/qaz7000810/tower/blob/main/%E8%A7%80%E5%85%89%E5%AE%A2%E5%85%A5%E5%9C%8B%E6%A9%9F%E5%A0%B4.csv")
+
 m = leafmap.Map(minimap_control=True)
+  
 m.add_basemap("OpenTopoMap")
 m.to_streamlit(height=500)
