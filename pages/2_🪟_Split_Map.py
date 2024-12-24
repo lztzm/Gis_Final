0d98e1a9650f190165eb001ffe59c466a9771ef5
lztzm <tsetimie@gmail.com>
2024-12-24 08:15:29 +0800

    Update 2_🪟_Split_Map.py

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -17,6 +17,8 @@ st.sidebar.image(logo)
 st.title("Split-panel Map")
 
 heat_data = pd.read_csv("https://raw.githubusercontent.com/lztzm/Gis_Final_Project/refs/heads/main/%E5%90%84%E5%8D%80%E6%99%AF%E9%BB%9E%E6%95%B8%E9%87%8F.csv")
+hotel = pd.read_csv("https://raw.githubusercontent.com/lztzm/Gis_Final_Project/refs/heads/main/%E9%85%92%E5%BA%97%E5%90%8D%E5%96%AE.csv")
+station = pd.read_csv("https://raw.githubusercontent.com/lztzm/Gis_Final_Project/refs/heads/main/%E8%BB%8A%E7%AB%99%E9%BB%9E%E4%BD%8D.csv")
 
 m = leafmap.Map()
 heatmap_layer = leafmap.folium.FeatureGroup(name="熱區地圖")
@@ -29,8 +31,26 @@ m.add_heatmap(
     radius=20,
 )
 
+m.add_points_from_xy(
+    hotel,
+    x="經度",
+    y="緯度",
+    spin=True,
+    add_legend=True,
+    name="Hotel",
+)
+
+m.add_points_from_xy(
+    station,
+    x="經度",
+    y="緯度",
+    spin=True,
+    add_legend=True,
+    name="Station",
+)
+
 m.split_map(
-    left_layer="OpenStreetsMap", right_layer="Heat map"
+    left_layer="Station", right_layer="Hotel"
 )
 m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
 
