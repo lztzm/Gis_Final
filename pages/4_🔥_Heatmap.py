eb83acb3fed2991a7d7111f55ead3f52153499a1
lztzm <tsetimie@gmail.com>
2024-12-24 18:12:00 +0800

    Update 4_🔥_Heatmap.py

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -29,34 +29,32 @@ heat_data = pd.read_csv("https://raw.githubusercontent.com/lztzm/Gis_Final_Proje
 
  # 初始化地圖
 m = leafmap.Map(center=map_center, zoom=12)
-
-    # 添加篩選後的點標記
- m.add_points_from_xy(
-     Hotel,  # 使用篩選後的資料
-     x="經度",
-     y="緯度",
-     spin=True,
-     add_legend=True,
- )
-
-    # 添加篩選後的熱區地圖
+# 添加篩選後的點標記
+m.add_points_from_xy(
+ Hotel,  # 使用篩選後的資料
+ x="經度",
+ y="緯度",
+ spin=True,
+ add_legend=True,
+)
+# 添加篩選後的熱區地圖
 m.add_heatmap(
-    heat_data,  # 使用篩選後的資料
-    latitude="緯度",
-    longitude="經度",
-    value="景點數量",
-    name="Heat map",
-    radius=30,
+ heat_data,  # 使用篩選後的資料
+ latitude="緯度",
+ longitude="經度",
+ value="景點數量",
+ name="Heat map",
+ radius=30,
 )
 
     # 添加定位功能
-    folium.plugins.LocateControl().add_to(m)
+folium.plugins.LocateControl().add_to(m)
 
     # 新增圖層控制
-    m.add_layer_control()
+m.add_layer_control()
 
     # 顯示地圖
-    m.to_streamlit(height=700)
+m.to_streamlit(height=700)
 
     ########################################################
 # 根據選擇的行政區顯示表格
