933107193b9094f2d4042f3c52b0bb7077c8eebf
lztzm <tsetimie@gmail.com>
2024-12-24 16:38:48 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -86,10 +86,11 @@ else:
         filtered_geojson_json,  # 使用篩選後的 GeoJSON 數據
         layer_name="行政區域",
         style={
-            "color": "blue",  # 邊界顏色
-            "weight": 2,      # 邊界寬度
-            "fillColor": "cyan",  # 填充顏色
-            "fillOpacity": 0.2,   # 填充透明度
+            "color": "grey",  # 邊界顏色
+            "weight": 1.5,      # 邊界寬度
+            "opacity": 0.5
+            "fillColor": "wihte",  # 填充顏色
+            "fillOpacity": 0.1,   # 填充透明度
         },
     )
 
