7df9ad58f06f2cf67030b443ea9ed6a062383471
lztzm <tsetimie@gmail.com>
2024-12-24 08:20:14 +0800

    Update 2_🪟_Split_Map.py

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -16,11 +16,15 @@ st.sidebar.image(logo)
 
 st.title("Split-panel Map")
 
+# 讀取數據
 heat_data = pd.read_csv("https://raw.githubusercontent.com/lztzm/Gis_Final_Project/refs/heads/main/%E5%90%84%E5%8D%80%E6%99%AF%E9%BB%9E%E6%95%B8%E9%87%8F.csv")
 hotel = pd.read_csv("https://raw.githubusercontent.com/lztzm/Gis_Final_Project/refs/heads/main/%E9%85%92%E5%BA%97%E5%90%8D%E5%96%AE.csv")
 station = pd.read_csv("https://raw.githubusercontent.com/lztzm/Gis_Final_Project/refs/heads/main/%E8%BB%8A%E7%AB%99%E9%BB%9E%E4%BD%8D.csv")
 
+# 創建地圖
 m = leafmap.Map()
+
+# 熱區圖層
 heatmap_layer = leafmap.folium.FeatureGroup(name="熱區地圖")
 m.add_heatmap(
     heat_data,
@@ -31,7 +35,8 @@ m.add_heatmap(
     radius=20,
 )
 
-m.add_points_from_xy(
+# 酒店圖層
+hotel_layer = m.add_points_from_xy(
     hotel,
     x="經度",
     y="緯度",
@@ -40,7 +45,8 @@ m.add_points_from_xy(
     name="Hotel",
 )
 
-m.add_points_from_xy(
+# 車站圖層
+station_layer = m.add_points_from_xy(
     station,
     x="經度",
     y="緯度",
@@ -49,8 +55,11 @@ m.add_points_from_xy(
     name="Station",
 )
 
+# 使用 split_map 顯示兩個圖層
 m.split_map(
-    left_layer="Station", right_layer="OpenStreetMap"
+    left_layer=station_layer,
+    right_layer=hotel_layer
 )
 
+# 顯示地圖
 m.to_streamlit(height=700)
